32a834eb842c9c9583126b96efff407bd38609fb
sigespweb22 <alan.rezendeeee@hotmail.com>
2024-03-02 14:12:23 -0300

    feat: add tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
+<script>
+	import '../app.pcss';
+</script>
+
 <nav>
 	<a href="/">Home</a>
-	<a href="/about">Perfil</a>
-	<a href="/settings">Resumo</a>
-	<a href="/settings">Contatos</a>
+	<a href="/perfil">Perfil</a>
+	<a href="/resumo">Resumo</a>
+	<a href="/contatos">Contatos</a>
 </nav>
 
-<slot></slot>
+<slot />
--- src/routes/resumo/notifications/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-	
-	export let data: PageData;
-	
-	console.log(data.sections); // [{ slug: 'profile', title: 'Profile' }, ...]
-</script>
-
-<h1>Notifications</h1>
